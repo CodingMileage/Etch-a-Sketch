f2fe2ad2cff0d45ea3361394b67bb80b969170c9
CodingMileage <[200~72987158+CodingMileage@users.noreply.github.com~>
2023-08-02 00:01:24 -0500

    Added ability to switch drawing modes. Can now click squares for precision or just hover to draw. Added a prompt to ask user for color. Color button changes to color selected. Added erase button that switches to default color to allow a square to return to original color. Added clear button to return all square to default color. Added grid size button that ask user for number. Work in progress.

--- app.js
+++ app.js
@@ -1,85 +1,82 @@
 let board = document.querySelector('.grid');
-board.style.gridTemplateColumns = "repeat(16, 1fr)";
-board.style.gridTemplateRows = "repeat(16, 1fr)";
+let size = 16;
+board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
-for (let i = 0; i < 256; i++) {
-    let square = document.createElement('box');
-    square.setAttribute('id', 'box');
-    board.insertAdjacentElement('beforeend', square);
+let color = 'black';
+let mode = 'mouseover';
+
+function addEventListenersToSquares() {
+  let squares = document.querySelectorAll('.box');
+  squares.forEach(square => {
+    square.removeEventListener('mouseover', handleMouseOver);
+    square.removeEventListener('click', handleClick);
+
+    if (mode === 'mouseover') {
+      square.addEventListener('mouseover', handleMouseOver);
+    } else if (mode === 'click') {
+      square.addEventListener('click', handleClick);
+    }
+  });
 }
 
+function handleMouseOver() {
+  this.style.backgroundColor = color;
+}
 
+function handleClick() {
+  this.style.backgroundColor = color;
+}
 
+function reset() {
+  let squares = document.querySelectorAll('.box');
+  squares.forEach(square => {
+    square.style.backgroundColor = 'darkslategray';
+  });
+}
 
+function changeColor(choice) {
+    let btn = document.querySelector('.color');
+    color = choice;
+    btn.style.backgroundColor = color;
+}
 
+function changeMode(pick) {
+  mode = pick;
+  console.log(mode);
+  addEventListenersToSquares();
+}
 
+function changeSize(input) {
+    size = input;
+    clearBoard();
+    grid();
+}
 
+function changeClick() {
+    
+}
 
+function clearBoard() {
+  // Remove all child elements from the board
+  while (board.firstChild) {
+    board.removeChild(board.firstChild);
+  }
+}
 
+function grid() {
+    board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
+    for (let i = 0; i < size * size; i++) {
+        let square = document.createElement('div');
+        square.classList.add('box'); // Add the 'box' class for styling
+        board.appendChild(square);
+    }
 
+    addEventListenersToSquares(); // Setup event listeners for the new squares
+}
 
+grid(); // Initial setup for the grid
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// // let resetButton = document.querySelector('.reset');
-
-// // function populateBoard(size) {
-// //     let board = document.querySelector('.board');
-// //     board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
-// //     board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
-
-// //     for (let i = 0; i < 256; i++) {
-// //         let square = document.createElement('box');
-// //         square.addEventListener('mouseover', () => {
-// //             square.style.backgroundColor = 'black';
-// //         });
-// //         // square.style.backgroundColor = 'blue';
-// //         board.insertAdjacentElement('beforeend', square);
-// //     }
-// // }
-
-// // populateBoard(16);
-
-// // // let m = document.querySelector('#boxx');
-// // // m.addEventListener('click', () => {
-// // //     square.style.backgroundColor = 'blue';
-// // // });
-
-// // function reset(size) {
-// //     let board = document.querySelector('.board');
-// //     board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
-// //     board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
-
-
-// //     for (let i = 0; i < 256; i++) {
-// //         let square = document.createElement('box');
-// //         square.addEventListener('mouseover', () => {
-// //             square.style.backgroundColor = 'black';
-// //         });
-// //         board.insertAdjacentElement('beforeend', square);
-// //     }
-// // }
-
-// // resetButton.addEventListener('click', reset());
-
+// Rest of your code remains unchanged
